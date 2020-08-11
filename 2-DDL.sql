e8476757325203c7a7cefb264a653e292e5b8d37
ahmetfarukulu <ahmetfarukulu@gmail.com>
2020-08-11 12:20:25 +0300

    DDL eklendi

---
+++ 2-DDL.sql
@@ -0,0 +1,17 @@
+--CREATE
+--DROP
+--ALTER
+--TRUNCATE 
+--COMMENT "--" yada "/* yorum satırları */" ile yorum satırı yapma
+--RENAME sp_rename ile yeniden adlandırma
+
+--[ddl kod] [obje türü] [obje ismi] [varsa parametreleri]
+
+CREATE DATABASE Test
+
+CREATE TABLE TblTest
+(
+	c1 varchar(50)
+)
+
+DROP DATABASE Test
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Basic
 ***SQL temellerine ait kısa örneklerden oluşan proje***
 - **1-DataTypes** veri tipleri ve değişken tanımlama
+- **2-DDL** veri tanımlama dili
